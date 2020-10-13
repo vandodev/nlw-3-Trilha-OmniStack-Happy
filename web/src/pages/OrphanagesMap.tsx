8fcafea906f3667383a58aa3932a162098e18fa5
Evandro oliveira <vandoaparecido@hotmail.com>
2020-10-13 12:20:06 -0300

    Alterando mapa para mapbox

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -28,8 +28,11 @@ function OrphanagesMap(){
                 center={[-20.7305509,-48.0491994]}
                 zoom={15}
                 style={{width:'100%', height:'100%'}}
-            >
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                  >
+               <TileLayer
+                     url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                /> 
+
             </Map>
 
             <Link to="" className="create-orphanage">
