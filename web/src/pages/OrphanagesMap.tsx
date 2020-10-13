c6a242753dcdc09e7c688b403a0612b2ee13bf14
Evandro oliveira <vandoaparecido@hotmail.com>
2020-10-13 11:29:10 -0300

    Criando e estilizando a pagina OrphanagesMap

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+import '../styles/pages/orphanages-map.css';
+import mapMarkerImg from '../images/map-marker.svg';
+
 function OrphanagesMap(){
-    return(
-        <h1>Olá mundo</h1>
+    return (
+        <div id="page-map">
+            <aside>
+
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando sua visita</p>
+                </header>
+
+                <footer>
+                    <strong>Morro Agudo</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff"/>
+            </Link>
+
+        </div>
+
     );
 }
-
-export default OrphanagesMap; 
+export default  OrphanagesMap; 
