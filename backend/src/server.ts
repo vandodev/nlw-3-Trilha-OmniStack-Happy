c722afbaca855de533f479c6197a9331ba7dcb09
Evandro oliveira <vandoaparecido@hotmail.com>
2020-10-14 02:43:27 -0300

    Inserindo dados na tabela orphanages

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,38 @@
 import express, { json } from 'express';
 import './database/connections';
+import {getRepository} from 'typeorm';
+import Orphanages from './models/Orphanage';
+import Orphanage from './models/Orphanage';
 
 const app= express();
 
 app.use(express.json());
 
-app.post('/orphanages',(request, response) => {
-    console.log(request.body);
+app.post('/orphanages', async(request, response) => {
+    const {
+        name, 
+        latitude,
+        longitude,
+        about, 
+        instructions,
+        opening_hours,
+        open_on_weekends,
+
+    } = request.body;
+const orphanagesRepository = getRepository(Orphanage);
+
+const orphanage = orphanagesRepository.create({
+    name, 
+    latitude,
+    longitude,
+    about, 
+    instructions,
+    opening_hours,
+    open_on_weekends,
+});
+
+await orphanagesRepository.save(orphanage);
+
     return response.json({message:'Olá mundo'});
 });
 
