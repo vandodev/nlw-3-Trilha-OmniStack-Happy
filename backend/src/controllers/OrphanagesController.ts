e480e56a2ff282194cf2cdbafba7982a019ba10f
Evandro oliveira <vandoaparecido@hotmail.com>
2020-10-14 10:55:33 -0300

    Trazendo imagens nas listagens geral ou por id

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -6,14 +6,22 @@ export default {
 
     async index(request:Request, response:Response){
         const orphanagesRepository = getRepository(Orphanage);
-        const orphanages = await orphanagesRepository.find();
+
+        const orphanages = await orphanagesRepository.find({
+            relations:['images']
+        });
+
         return response.json(orphanages);
     },
 
     async show(request:Request, response:Response){
         const {id} = request.params;
         const orphanagesRepository = getRepository(Orphanage);
-        const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+        const orphanage = await orphanagesRepository.findOneOrFail(id,{
+            relations:['images']
+        });
+
         return response.json(orphanage);
     },
 
