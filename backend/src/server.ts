10e7bbc8f7f755fe5af369b45028ab7d77de16da
Evandro oliveira <vandoaparecido@hotmail.com>
2020-10-14 00:34:06 -0300

    Primeiro exemplo de rota

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,11 @@
-import express from 'express';
+import express, { json } from 'express';
 
 const app= express();
+
+app.get('/users',(request, response) => {
+    //console.log('Teste');
+    //return response.send('Olá mundo');
+    return response.json({message:'Olá mundo'});
+});
 
 app.listen(3333);
