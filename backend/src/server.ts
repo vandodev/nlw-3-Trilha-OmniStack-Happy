f4ed01f77ce752b60e0837db2ae33cd39fb6430f
Evandro oliveira <vandoaparecido@hotmail.com>
2020-10-14 12:42:31 -0300

    Trabalhando com exceções

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,15 @@
 import express, { json } from 'express';
 import './database/connections';
-import routes from './routes';
 import path from 'path';
+import 'express-async-errors';
+import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app= express();
 
 app.use(express.json());
 app.use(routes);
-app.use('/uploads', express.static(path.join(__dirname, '..','uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '..','uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
