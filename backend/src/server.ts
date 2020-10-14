64ffca20b536ad2d1bb4622b735b2a1a599be8ab
Evandro oliveira <vandoaparecido@hotmail.com>
2020-10-14 03:26:51 -0300

    Separando em rotas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,39 +1,10 @@
 import express, { json } from 'express';
 import './database/connections';
-import {getRepository} from 'typeorm';
-import Orphanages from './models/Orphanage';
-import Orphanage from './models/Orphanage';
+import routes from './routes';
 
 const app= express();
 
 app.use(express.json());
-
-app.post('/orphanages', async(request, response) => {
-    const {
-        name, 
-        latitude,
-        longitude,
-        about, 
-        instructions,
-        opening_hours,
-        open_on_weekends,
-
-    } = request.body;
-const orphanagesRepository = getRepository(Orphanage);
-
-const orphanage = orphanagesRepository.create({
-    name, 
-    latitude,
-    longitude,
-    about, 
-    instructions,
-    opening_hours,
-    open_on_weekends,
-});
-
-await orphanagesRepository.save(orphanage);
-
-    return response.json({message:'Olá mundo'});
-});
+app.use(routes);
 
 app.listen(3333);
