a911f159ec6fd1585e88465c569002d487aa2a19
Evandro oliveira <vandoaparecido@hotmail.com>
2020-10-15 09:36:28 -0300

    Criando/estilizando página de criação de orfanato e pagina de detalhe do orfanato

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -49,7 +49,7 @@ function OrphanagesMap(){
                 >
                      <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
                         Lar das meninas
-                        <Link to=""> 
+                        <Link to="/orphanages/1"> 
                             <FiArrowRight size={20} color="#fff"/>
                         </Link>
                      </Popup>
@@ -60,7 +60,7 @@ function OrphanagesMap(){
 
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#fff"/>
             </Link>
 
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import {BrowserRouter, Switch,Route} from 'react-router-dom';
 
 import Landing from './pages/Landing';
-import OrphanagesMap from './pages/OrphanagesMap'
+import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
+
 
 
 function Routes(){
@@ -11,7 +14,9 @@ function Routes(){
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Landing}/>
-                <Route path="/app" component={OrphanagesMap}/>   
+                <Route path="/app" component={OrphanagesMap}/>  
+                <Route path="/orphanages/create" component={CreateOrphanage} />
+                <Route path="/orphanages/:id" component={Orphanage} /> 
             </Switch>         
         </BrowserRouter>
 
