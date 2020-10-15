789b7f5c849d93133637d37046174f5bab44f842
Evandro oliveira <vandoaparecido@hotmail.com>
2020-10-15 09:16:07 -0300

    Criando popup estático

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 import {Link} from 'react-router-dom';
-import {FiPlus} from 'react-icons/fi';
+import {FiPlus,FiArrowRight} from 'react-icons/fi';
 import '../styles/pages/orphanages-map.css';
 import mapMarkerImg from '../images/map-marker.svg';
 
-import {Map, TileLayer, Marker} from 'react-leaflet';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'; 
-import Leaflet from 'leaflet';
+import Leaflet, { popup } from 'leaflet';
 
 const mapIcon = Leaflet.icon({
     iconUrl: mapMarkerImg,
     iconSize:[58,68],
     iconAnchor:[29,68],
+    popupAnchor:[170,2]
     
 })
 
@@ -42,10 +43,20 @@ function OrphanagesMap(){
                      url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 /> 
                 
-                <Marker 
+                <Marker
                     icon={mapIcon}
-                     position={[-20.7305509,-48.0491994]}
-                />
+                    position={[-20.7305509,-48.0491994]}
+                >
+                     <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar das meninas
+                        <Link to=""> 
+                            <FiArrowRight size={20} color="#fff"/>
+                        </Link>
+                     </Popup>
+
+                </Marker>
+                    
+                               
 
             </Map>
 
