ada62b6c8ffb6660f2f606fa1baac515699470ba
Evandro oliveira <vandoaparecido@hotmail.com>
2020-10-15 08:38:46 -0300

    Colocando marcador no mapa

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -5,8 +5,17 @@ import {FiPlus} from 'react-icons/fi';
 import '../styles/pages/orphanages-map.css';
 import mapMarkerImg from '../images/map-marker.svg';
 
-import {Map, TileLayer} from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+import {Map, TileLayer, Marker} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; 
+import Leaflet from 'leaflet';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize:[58,68],
+    iconAnchor:[29,68],
+    
+})
+
 function OrphanagesMap(){
     return (
         <div id="page-map">
@@ -32,6 +41,11 @@ function OrphanagesMap(){
                <TileLayer
                      url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 /> 
+                
+                <Marker 
+                    icon={mapIcon}
+                     position={[-20.7305509,-48.0491994]}
+                />
 
             </Map>
 
