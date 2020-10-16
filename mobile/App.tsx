3a3eecb0314771ce05bc926c22b3d6b8585e8139
Evandro oliveira <vandoaparecido@hotmail.com>
2020-10-16 08:31:12 -0300

    Adicionando mapa

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <MapView 
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude:-20.7305509,
+          longitude: -48.0491994,
+          latitudeDelta:0.008,
+          longitudeDelta: 0.008,
+        }}
+      />
+
     </View>
   );
 }
@@ -14,8 +24,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
